07c121aebcc8ad7b27769fc68efd3fffa8c911ff
Wang Yafeng <LabUser1@ST-POM-LAB-NB76>
2019-06-18 14:18:04 +0800

    commit

--- SpringBootJwtAuthentication/src/main/java/com/grokonez/jwtauthentication/model/Category.java
+++ SpringBootJwtAuthentication/src/main/java/com/grokonez/jwtauthentication/model/Category.java
@@ -1,6 +1,8 @@
 package com.grokonez.jwtauthentication.model;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.List;
 
--- SpringBootJwtAuthentication/src/main/java/com/grokonez/jwtauthentication/model/Order.java
+++ SpringBootJwtAuthentication/src/main/java/com/grokonez/jwtauthentication/model/Order.java
@@ -6,7 +6,7 @@ import java.util.Date;
 import java.util.List;
 
 @Entity
-@Table(name = "category")
+@Table(name = "order")
 public class Order {
 
     @Id
