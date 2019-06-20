2583d33c81d61b7b6d98119e23793e8f1e40b28b
Wang Yafeng <LabUser1@ST-POM-LAB-NB76>
2019-06-20 14:45:47 +0800

    complete category list

--- SpringBootJwtAuthentication/src/main/java/com/grokonez/jwtauthentication/controller/CategoryController.java
+++ SpringBootJwtAuthentication/src/main/java/com/grokonez/jwtauthentication/controller/CategoryController.java
@@ -5,14 +5,16 @@ import com.grokonez.jwtauthentication.service.CategoryService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping(value = "/categories")
+@RequestMapping(value = "api/categories")
 @Api(tags = { "Category Controller" })
 public class CategoryController {
     @Autowired
